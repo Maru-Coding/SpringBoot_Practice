b4119ee1d784a0b40bf9a6599149b2aeac36f0b0
Maru <partnersoul12@gmail.com>
2024-12-08 15:48:35 +0900

    기본 틀 구성

--- src/main/java/com/example/SpringBoot_Practice/controller/TestController.java
+++ src/main/java/com/example/SpringBoot_Practice/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.SpringBoot_Practice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
     @GetMapping("/test")
-    public String show(){
+    public String show(Model model){
+        model.addAttribute("name", "마루");
         return "test";
     }
 
